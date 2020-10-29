4aba11c026e25efece49290a9adb0466ffbaf7ee
sarguna07 <sarguna.k@katomaran.com>
2020-10-29 19:09:55 +0530

    Initial commit

---
+++ precache-manifest.c951407f0ecaf8eb814af394d58ed520.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4211bc4ddb061817fe1b045f252d3dac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97f971b219eebeab622f",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "62569f5aae46401419f9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "97f971b219eebeab622f",
+    "url": "/static/js/2.b20a8db5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.b20a8db5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62569f5aae46401419f9",
+    "url": "/static/js/main.2fe5934b.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "7a4749945a53787d4b0b252593200fe2",
+    "url": "/static/media/kato.7a474994.png"
+  },
+  {
+    "revision": "5a9c2d7db37da8abe8ec71a2fc280219",
+    "url": "/static/media/sarguna.5a9c2d7d.jpg"
+  }
+]);
